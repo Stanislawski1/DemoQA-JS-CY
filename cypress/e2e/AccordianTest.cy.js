87174131b7a18b97a677e38443b0265502eb616e
Stanislaw <stasgolovnev22@gmail.com>
2026-01-05 14:19:48 +0100

    Added new tests n pages

---
+++ cypress/e2e/AccordianTest.cy.js
@@ -0,0 +1,12 @@
+import AcordianPage from './pages/AcordianPage';
+
+describe("Проверка страницы Accordian", () => {
+
+    it('Проверка наличия динамических страниц по тексту', () => {
+        cy.ignoreErrors()
+        cy.visit('https://demoqa.com/accordian')
+        AcordianPage.checkOfFirstAccordian
+        AcordianPage.checkOfSecondAccordian
+        AcordianPage.checkOfThreeAccordian
+    });
+});
